e5a1ddc01f32892adf0b6907e54b1b4598dbfacc
simao.lopes98 <simaolopes98@gmail.com>
2025-05-11 14:04:52 +0100

    Upate main and common

--- include/common.h
+++ include/common.h
@@ -1,4 +1,6 @@
 // common.h
+#ifndef COMMON_H
+#define COMMON_H
 
 /*
 
@@ -10,7 +12,13 @@ Common definitions
 #define ERROR (-1)
 #define OK (0)
 #define LOCAL static
+#define IMPORT extern
 
 // Typedefs
 typedef int STATUS;              // Status
+typedef __uint8_t UINT8;
+typedef __uint16_t UINT16;
+typedef __uint32_t UINT32;
+typedef __uint64_t UINT64;
 
+#endif /* COMMON_H */
--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@ Program starting point
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include "common.h"
+#include "tasks.h"
 
 // Forward declarations
 int main (void);
@@ -20,37 +21,11 @@ LOCAL void timerSetup (void);
 // Design globals
 LOCAL int ledToggle = 0;
 
-// Timer 1 ISR
-ISR(TIMER1_COMPA_vect)
-{
-    ledToggle = 1;      // Set flag
-}
-
-// Timer 1 A setup
-LOCAL void timerSetup (void)
-{
-    // Reset registers
-    TCCR1A = 0;
-    TCCR1B = 0;
-    TCNT1 = 0;
-
-    // Set Prescaler (1024) and CTC mode
-    TCCR1B |= (1 << WGM12) | (1 << CS12) | (1 << CS10);  // 0x0D
-
-    // Value to compare: 15624 DEC-HEX-> 0x3D08
-    OCR1A = 0x3D08;
-
-    // Set the bit 2 of TIMSK1 - Compare Interrupt Enable
-    TIMSK1 |= (1 << OCIE1A);
-
-    // Enable ISR
-    sei();
-}
-
 // Main
 int main (void)
 {
     DDRB |= (1 << 4); // Port B configuration
+    
     
     timerSetup();
     
