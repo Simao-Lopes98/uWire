5a63f143e89a2b86c50d4a440be6f696cae66fe1
simao.lopes98 <simaolopes98@gmail.com>
2025-05-17 17:34:57 +0100

    Update serial.c style

--- serial/serial.c
+++ serial/serial.c
@@ -10,14 +10,14 @@ FILE uart_stdout = FDEV_SETUP_STREAM(uart_putc, NULL, _FDEV_SETUP_WRITE);
 
 LOCAL void uart_init(unsigned int ubrr) {
     // Set baud rate
-    UBRR0H = (unsigned char)(ubrr>>8);
+    UBRR0H = (unsigned char)(ubrr >> 8);
     UBRR0L = (unsigned char)ubrr;
     
     // Enable transmitter
-    UCSR0B = (1<<TXEN0);
+    UCSR0B = (1 << TXEN0);
     
     // Set frame format: 8 data bits, 1 stop bit
-    UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
 LOCAL void uart_putchar(char c) {
